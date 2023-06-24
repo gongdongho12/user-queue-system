05b776d72ff9a31e9902f858b0d557145c404e1f
Teddy <teddy.ryan@kakaostyle.com>
2023-06-25 01:08:50 +0900

    chore: graphql 접속 루트 추가

--- uqs-api/src/main/kotlin/com/dongholab/uqs/api/configuration/SpringSecurityConfig.kt
+++ uqs-api/src/main/kotlin/com/dongholab/uqs/api/configuration/SpringSecurityConfig.kt
@@ -32,6 +32,8 @@ class SpringSecurityConfig(
                             "/",
                             "/swagger-ui/**",
                             "/v3/api-docs/**",
+                            "/graphql/**",
+                            "/graphiql/**",
                             "/hello",
                             "/eks-hello",
                             "/images/**",
