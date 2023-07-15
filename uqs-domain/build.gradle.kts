e991da33f261c65e7a27d05904c4d7fafe2b553a
Teddy <teddy.ryan@kakaostyle.com>
2023-07-16 05:00:06 +0900

    feat: 기본적인 웹소켓 설정 반영

--- uqs-domain/build.gradle.kts
+++ uqs-domain/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-jdbc")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
---
+++ uqs-domain/src/main/kotlin/com/dongholab/uqs/domain/configuration/WebSocketConfiguration.kt
@@ -0,0 +1,21 @@
+package com.dongholab.uqs.domain.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfiguration : WebSocketMessageBrokerConfigurer {
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/ws").withSockJS()
+    }
+
+    override fun configureMessageBroker(config: MessageBrokerRegistry) {
+        config.setApplicationDestinationPrefixes("/app")
+        config.enableSimpleBroker("/topic")
+    }
+}
